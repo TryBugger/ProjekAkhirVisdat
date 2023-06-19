6d4cc4fa40555d4f5697087b41975571e97887c3
TryBugger <mtaqiyyuddinf@gmail.com>
2023-06-19 18:48:46 +0700

    Update visualisasi untuk top terbanyak dan tersedikit rating, keseluruhan rating antar kota, serta pariwisata berdasarkan kategori

--- streamlit.py
+++ streamlit.py
@@ -1,8 +1,12 @@
+import fractions
 import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import rc
 import seaborn as sns
+
+# rc('font',**{'family':'sans-serif','sans-serif':['Lato']})
 
 @st.cache_data
 def load_rating_data(nrows=9999999):
@@ -37,34 +41,41 @@ st.write(data_destination_simple)
 st.markdown("""---""")
 
 ### Daftar Top 10 Wisata berdasarkan Rating
-kota="Jakarta"
 st.header("Daftar 10 Teratas dan 10 Terbawah Wisata Masing-Masing Kota Berdasarkan Rating")
-st.subheader(kota)
+# kota="Jakarta"
+st.write('<style>div.row-widget.stRadio > div{flex-direction:row;justify-content: center;} </style>', unsafe_allow_html=True)
+st.write('<style>div.st-bf{flex-direction:column;} div.st-ag{font-weight:bold;padding-left:2px;}</style>', unsafe_allow_html=True)
+kota = st.radio(
+    "Pilih Kota Destinasi Pariwisata",
+    data_destination['City'].unique()
+)
 
-# data_top_rating_kota = data_destination_simple.loc[data_destination_simple['City'] == kota].sort_values(by=['Rating'], ascending=False).reset_index().head(10)
 df_top10_pariwisata = data_destination[data_destination['City'] == kota].sort_values(by=['Rating'], ascending=False).reset_index(drop=True)
-df_top10_pariwisata = pd.concat([df_top10_pariwisata.head(10), df_top10_pariwisata.tail(10)])
 
-fig, ax = plt.subplots(1, 1, figsize=[14, 8])
+fig, ax = plt.subplots(1, 1, figsize=[20, 14])
 green_gradient = np.linspace(0.4, 1, 10)[::-1]
 red_gradient = np.linspace(0.4, 1, 10)
 
-ax.bar(df_top10_pariwisata.loc[:10, 'Place_Name'], df_top10_pariwisata.loc[:10, 'Rating'], 
-       color=plt.cm.Greens(green_gradient))
-ax.bar(df_top10_pariwisata.loc[10:, 'Place_Name'], df_top10_pariwisata.loc[10:, 'Rating'], 
-       color=plt.cm.Reds(red_gradient))
+ax.bar(df_top10_pariwisata[:10]['Place_Name'], df_top10_pariwisata[:10]['Rating'], 
+       color=plt.cm.Greens(green_gradient), label='_nolegend_')
+ax.bar(df_top10_pariwisata[-10:]['Place_Name'], df_top10_pariwisata[-10:]['Rating'], 
+       color=plt.cm.Reds(red_gradient), label='_nolegend_')
+ax.axhline(df_top10_pariwisata['Rating'].mean(), color='dodgerblue',
+           linewidth=5, label='Rata-rata')
+
 ax.tick_params(axis='x', labelrotation = -90)
-ax.set_ylabel('Rating', fontsize=18)
-ax.set_xlabel('Nama Pariwisata', fontsize=18)
-ax.set_title('Daftar Pariwisata Berdasarkan Rating', fontsize=28)
+ax.set_ylabel('Rating', fontsize=24)
+ax.set_xlabel('Nama Pariwisata', fontsize=24)
+ax.set_title(f'Daftar Pariwisata Kota {kota} Berdasarkan Rating', fontsize=32)
+ax.legend(fontsize=22, frameon=False)
 
 st.pyplot(fig)
 st.markdown("""---""")
 
-### Daftar Data Wisata
+### Daftar Persebaran Rating Pengunjung Masing-Masing Kota
 
-st.header("Visualisasi Rating Pengunjung Masing-Masing Kota")
-st.subheader("Persebaran Rating Kota Jakarta")
+st.header("Persebaran Rating Pengunjung Masing-Masing Kota")
+st.subheader("Kota Jakarta")
 
 data_destination_simple = data_destination.loc[:, 'Place_Id':'Time_Minutes']
 # st.write(data_destination_simple[data_destination_simple['City'] == 'Jakarta'])
@@ -82,36 +93,38 @@ ax.set_xlabel('Rating Kota')
 st.pyplot(fig)
 st.markdown("""---""")
 
-st.header("Total Keseluruhan Rating Antar Kota")
 
-data_rating_antar_kota = data_destination_simple[['City', 'Rating']].groupby('City').sum()
-# st.write(data_rating_antar_kota)
+### Total Keseluruhan Rating Setiap Kota
 
-fig, ax = plt.subplots(1, 1)
+st.header("Total Keseluruhan Rating Setiap Kota")
 
-data_rating_antar_kota.plot(kind='bar', title='Total Rating Antar Kota',
-                            ylabel='Total Rating', xlabel='Kota Wisata', figsize=[10, 6],
-                            ax=ax)
+data_kota_total_rating = data_destination.loc[:, ['City', 'Rating']].groupby('City').sum().reset_index()
+
+fig, ax = plt.subplots(1, 1, figsize=[8, 6])
+colors = ['steelblue' if (city == 'Jakarta') else 'lightgrey' for city in data_kota_total_rating['City']]
+
+sns.barplot(data=data_kota_total_rating,
+            x='City', y='Rating', palette=colors, 
+            ax=ax)
+ax.set_title(f'Total Rating Setiap Kota', fontsize=18)
+ax.set_ylabel('Total Rating', fontsize=14)
+ax.set_xlabel('Nama Kota', fontsize=14)
+
 st.pyplot(fig)
-
-# st.text('Rata-rata Rating Antar Kota')
-# st.write(data_rating_antar_kota)
-
-# fig, ax = plt.subplots(1, 1)
-
-# data_rating_antar_kota.plot(kind='bar', title='Rata-Rata Rating Antar Kota',
-#                             ylabel='Rata-rata Rating', xlabel='Kota Wisata', figsize=[10, 6],
-#                             ax=ax)
-# st.pyplot(fig)
 st.markdown("""---""")
 
-st.header("Rata-rata Tempat Wisata Berdasarkan Kategori Wisata")
+### Rata-Rata Rating Tempat Wisata Berdasarkan Kategori
 
-data_rating_kategori = data_destination_simple[['Category', 'Rating']].groupby('Category').mean().sort_values(by=['Rating'], ascending=False)
+st.header("Total Rating Tempat Wisata Berdasarkan Kategori Wisata")
 
-fig, ax = plt.subplots(1, 1)
+data_kategori_total_rating = data_destination.loc[:, ['Category', 'Rating']].groupby('Category').sum().sort_values(by='Rating', ascending=False).reset_index()
+fig, ax = plt.subplots(1, 1, figsize=[12, 8])
+colors = ['mediumseagreen'] + ['lightgrey']*(len(data_kategori_total_rating) - 2) + ['indianred']
 
-data_rating_kategori.plot(kind='bar', title='Total Rating Antar Kota',
-                          ylabel='Rata-rata Rating', xlabel='Kategori Tempat Wisata', figsize=[10, 6],
-                          ax=ax)
+sns.barplot(data=data_kategori_total_rating,
+            x='Category', y='Rating', palette=colors, 
+            ax=ax)
+ax.set_title(f'Total Rating Kategori Wisata', fontsize=22)
+ax.set_ylabel('Total Rating', fontsize=16)
+ax.set_xlabel('Nama Kategori', fontsize=16)
 st.pyplot(fig)
